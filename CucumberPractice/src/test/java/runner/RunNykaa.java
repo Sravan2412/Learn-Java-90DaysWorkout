2a6e28b7acfc0a0d9b368a22818eec89126edece
Sravan2412 <65245859+Sravan2412@users.noreply.github.com>
2020-06-09 01:18:15 +0530

    Run Nykaa

---
+++ CucumberPractice/src/test/java/runner/RunNykaa.java
@@ -0,0 +1,14 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions( features = {"src/test/java/features/Nykaa.feature"},
+				  glue = {"stepDefinitions"},
+				  snippets = SnippetType.CAMELCASE,
+				  monochrome = true,
+				  plugin = {"pretty", "html:reports"})
+public class RunNykaa extends AbstractTestNGCucumberTests{
+
+}
