c9904d5e30ac7dfdba117312502c25588ab32846
Sravan2412 <65245859+Sravan2412@users.noreply.github.com>
2020-06-09 23:55:16 +0530

    Hooks

--- CucumberPractice/src/test/java/stepDefinitions/Hooks.java
+++ CucumberPractice/src/test/java/stepDefinitions/Hooks.java
@@ -11,10 +11,13 @@ public class Hooks extends BaseClass {
 
 	@Before
 	public void login() {
+	options = new ChromeOptions();
+	options.addArguments("--disable-notifications");	
 	System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-	driver = new ChromeDriver();
+	driver = new ChromeDriver(options);
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 	
 	}
 	
